8d3df70f841b79e5c8ef51309ad4df6ea4da0723
unknown <bostbotbeet@gmail.com>
2022-11-29 12:04:41 +0400

    add show button

--- src/blocks/modules/checkbox/checkbox.js
+++ src/blocks/modules/checkbox/checkbox.js
@@ -1,13 +1,15 @@
-var boxes = document.getElementsByClassName('js-show-block');
-var height = 0;
+window.onload = function() {
+    var boxes = document.getElementsByClassName('js-height');
+    var height = 0;
 
-for( var i = 0; i < boxes.length; i++ ){
-    var current_height = boxes[i].offsetHeight;
-    if(current_height > height) {
-        height = current_height;
-    }        
-}
+    for( var i = 0; i < boxes.length; i++ ){
+        var current_height = boxes[i].offsetHeight;
+        if(current_height > height) {
+            height = current_height;
+        }        
+    }
 
-for( var i = 0; i < boxes.length; i++ ){
-    boxes[i].style.height = height + 'px';       
-}
+    for( var i = 0; i < boxes.length; i++ ){
+        boxes[i].style.height = height + 'px';       
+    }
+};
--- src/blocks/modules/table/table.js
+++ src/blocks/modules/table/table.js
@@ -1,7 +1,32 @@
+// star
 var stars = document.getElementsByClassName("js-star");
-
 for (var i = 0, len = stars.length; i < len; i++) {
     stars [i].addEventListener("click", function() {
         this.classList.toggle("active");
+    });
+}
+// chexbox
+const mainCheckbox = document.getElementById('js-checkAll')
+mainCheckbox.addEventListener('click', (e) => {
+    const inputs = document.querySelectorAll('.check__input');
+    for (const input of inputs) {
+        input.checked = e.target.checked
+    }
+});
+// open show
+const buttons = document.getElementsByClassName("table__but-toggle");
+const firstshow = document.getElementsByClassName("js-show-block");
+const secondshow = document.getElementsByClassName("table__show");
+
+
+for (var i = 0, len = buttons.length; i < len; i++) {
+    getIndex(i)
+}
+
+function getIndex (i) {
+    buttons [i].addEventListener("click", function(e) {
+        this.classList.toggle("active");
+        secondshow[i].classList.toggle("active");
+        firstshow[i].classList.toggle("active");
     });
 }
